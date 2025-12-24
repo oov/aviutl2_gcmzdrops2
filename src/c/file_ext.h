40ef5738a31d920debc5d4c7e72780a8cf5080ab
oov <ilovevipper@gmail.com>
2025-12-24 10:53:14 +0900

    refactoring

--- src/c/file_ext.h
+++ src/c/file_ext.h
@@ -3,6 +3,13 @@
 #include <stdbool.h>
 #include <wchar.h>
 
+static inline wchar_t to_lower(wchar_t c) {
+  if (c >= L'A' && c <= L'Z') {
+    return c | 0x20;
+  }
+  return c;
+}
+
 /**
  * @brief Case-insensitive ASCII comparison for extension strings
  *
@@ -18,33 +25,20 @@
  * @note This function compares the entire strings, not just suffixes
  *
  * @example
- *   extension_equals(L".TXT", L".txt")  // returns true
- *   extension_equals(L".txt", L".TXT")  // returns true
- *   extension_equals(L".doc", L".txt")  // returns false
- *   extension_equals(NULL, L".txt")     // returns false
+ *   gcmz_extension_equals(L".TXT", L".txt")  // returns true
+ *   gcmz_extension_equals(L".txt", L".TXT")  // returns true
+ *   gcmz_extension_equals(L".doc", L".txt")  // returns false
+ *   gcmz_extension_equals(NULL, L".txt")     // returns false
  */
 static inline bool gcmz_extension_equals(wchar_t const *ext1, wchar_t const *ext2) {
   if (!ext1 || !ext2) {
     return false;
   }
-
   for (; *ext1 != L'\0' && *ext2 != L'\0'; ++ext1, ++ext2) {
-    wchar_t c1 = *ext1;
-    wchar_t c2 = *ext2;
-
-    // Convert to lowercase if ASCII uppercase
-    if (c1 >= L'A' && c1 <= L'Z') {
-      c1 |= 0x20;
-    }
-    if (c2 >= L'A' && c2 <= L'Z') {
-      c2 |= 0x20;
-    }
-
-    if (c1 != c2) {
+    if (to_lower(*ext1) != to_lower(*ext2)) {
       return false;
     }
   }
-
   // Both strings must end at the same time
   return *ext1 == L'\0' && *ext2 == L'\0';
 }
